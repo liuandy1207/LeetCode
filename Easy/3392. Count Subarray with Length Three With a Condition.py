07ce13c328fa9215e2ca3482e77b9ffcf84a6113
He Ming (Andy) Liu <liuandy1207@gmail.com>
2025-05-06 11:46:08 -0400

    Create 3392. Count Subarray with Length Three With a Condition.py

---
+++ Easy/3392. Count Subarray with Length Three With a Condition.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        for i in range(1, len(nums) - 1):
+            if 2 * (nums[i - 1] + nums[i + 1]) == nums[i]: count+= 1
+        return count
